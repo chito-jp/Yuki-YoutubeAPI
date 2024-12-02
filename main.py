80a018c16bb10c2989d5aaea97b0d3a38a58bc53
ちと <chitose.dev@outlook.jp>
2024-12-02 23:54:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -31,6 +31,32 @@ def api_request(url):
         except:
             print(f"apiエラー : {api}")
     raise TimeoutError("APIがタイムアウトしました")
+
+def get_video(video_id):
+    t = api_request(f"/api/v1/videos/{video_id}")
+    
+    related_videos = [
+        {
+            "id": i["videoId"],
+            "title": i["title"],
+            "authorId": i["authorId"],
+            "author": i["author"],
+            "viewCount": i["viewCount"]
+        }
+        for i in t["recommendedVideos"]
+    ]
+
+    # 辞書形式で結果を返す
+    return {
+        "related_videos": related_videos,
+        "stream_urls": list(reversed([i["url"] for i in t["formatStreams"]]))[:2],  # 逆順で2つのストリームURLを取得
+        "description": t["descriptionHtml"].replace("\n", "<br>"),  # 説明文に改行を追加
+        "title": t["title"],  # 動画タイトル
+        "author_id": t["authorId"],  # 作者ID
+        "author_name": t["author"],  # 作者名
+        "author_thumbnail": t["authorThumbnails"][-1]["url"],  # 最後のサムネイルURL
+        "view_count": t["viewCount"]  # 動画の再生回数を追加
+    }
     
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
@@ -43,7 +69,6 @@ templates = Jinja2Templates(directory="templates")
 max_api_wait_time = 5
 max_time = 15
 
-
 @app.get("/test", response_class=JSONResponse)
 def get_apis():
     data = load_json_data("apis.json")
@@ -54,6 +79,6 @@ def home(request: Request):
     return templates.TemplateResponse("dummy.html", {"request": request})
 
 @app.get("/api/video/{video_id}", response_class=JSONResponse)
-def api_get_video(video_id:str):
-    t = api_request(f"/api/v1/videos/{video_id}")
-    return JSONResponse(content=json.loads(t))
+async def api_get_video(video_id: str):
+    t = await get_video(video_id)
+    return JSONResponse(content=t)
