c6128d2dcbe34a06f8135f68147bd66c87f1504b
ちと <chitose.dev@outlook.jp>
2024-12-02 22:53:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse,JSONResponse
+import json
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/test")
-async def get_test():
-    return {"Test": "成功"}
+def load_json_data(filename: str):
+    with open(filename, "r") as file:
+        data = json.load(file)
+    return data
+
+@app.get("/test", response_class=JSONResponse)
+def get_apis():
+    data = load_json_data("apis.json")
+    return JSONResponse(content=data)
 
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
