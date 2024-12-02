c7fef25c00e79d87f9cbc1a5955c953c04f47e71
ちと <chitose.dev@outlook.jp>
2024-12-02 22:44:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/test")
 async def get_test():
     return {"Test": "成功"}
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("dummy.html", {"request": request})
